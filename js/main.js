f82c6c5c3c8c12a4f16de03c97d5ab1d11431417
Shirley Ramos <shirleyramos12@gmail.com>
2021-08-22 17:37:03 -0300

    ajustando campos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 $('.slider-principal').slick({
-	dots: true,
-	infinite: true,
-	speed:300,
-	slidesToShow:1,
-	adaptiveHeight:  true,
-	autoplay: true,
-	autoplaySpeed:2000
-	
-	
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
 });
